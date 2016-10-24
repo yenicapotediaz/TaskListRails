5d8e330cd9415d066a4ac90c1309aa017d80827d
Yeni <yenicapote08@gmail.com>
2016-10-24 01:46:09 -0700

    added tests for sessions controller, added fixtures to tasks, and added test mode to test_helper.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_a_user
+     request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+     get :create,  {provider: "github"}
+  end
+
+  test "Can Create a user" do
+    assert_difference('User.count', 1) do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to tasks_path
+    end
+  end
+
+   test "If a user logs in twice it doesn't create a 2nd user" do
+     assert_difference('User.count', 1) do
+       login_a_user
+     end
+     assert_no_difference('User.count') do
+       login_a_user
+       assert_response :redirect
+       assert_redirected_to tasks_path
+       assert_not_nil session[:user_id]
+     end
+   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,13 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  Minitest::Reporters.use!
+  def setup
 
-  # Add more helper methods to be used by all tests here...
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github', uid: '123545', info: { email: "a@b.com", name: "Ada" }
+      })
+  end
 end
