2306a69c5a71f0e28c1163eb37d3364b87531b8e
Yeni <yenicapote08@gmail.com>
2016-10-24 00:02:58 -0700

    added current_user and require_login private methods to application controller and before action to prevent unauthenticated users from accessing anything but the login page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  private
+  def current_user
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this section"
+      redirect_to login_path
+    end
+  end
 end
