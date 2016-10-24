cb58aa9580c6e37231f2840afa1714eb63246e45
Yeni <yenicapote08@gmail.com>
2016-10-23 23:58:27 -0700

    updated sessions controller to have login, create and logout methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,23 @@
 class SessionsController < ApplicationController
-  def index
-    if session[:user_id].nil?
-      redirect_to login_failure_path
-    else
-      @user = User.find(session[:user_id])
-    end
-  end
+skip_before_action :require_login, only: [:login, :create]
 
-  def destroy
-    session.delete(:user_id)
-    redirect_to login_failure_path
-  end
+  def login; end
+
   def create
     auth_hash = request.env['omniauth.auth']
     redirect_to login_failure_path unless auth_hash['uid']
 
     @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
     if @user.nil?
-      # User doesn't match anything in the DB.
-      # Attempt to create a new user.
       @user = User.build_from_github(auth_hash)
       render :creation_failure unless @user.save
     end
 
-    # Save the user ID in the session
     session[:user_id] = @user.id
-
-    redirect_to sessions_path
+    redirect_to tasks_path
   end
 
-  def login_failure
+  def logout
+    session.delete(:user_id)
   end
 end
